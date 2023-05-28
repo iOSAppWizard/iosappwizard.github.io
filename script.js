73939dee8b6e8e961b49eb6ae5c97d8efbfc2e8f
Krishna Panchal <124603758+iOSAppWizard@users.noreply.github.com>
2023-05-28 15:32:40 +0530

    Github.io Initial Commit
    Setting up Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Blogger", "Canva Designer", "Freelancer","iOSAppWizard"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
